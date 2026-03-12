d4a6866196fcced2742d967ead1de5000983848c
Jaehong Lee <ja34luv@gmail.com>
2026-03-12 14:32:37 -0700

    feat: disable focus ring and boxshadow by default

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ function App() {
         <div className="absolute inset-0 bg-size-[105px_105px] bg-[linear-gradient(90deg,rgba(0,0,0,.55)_1px,transparent_0),linear-gradient(180deg,rgba(0,0,0,.55)_1px,transparent_0)] opacity-10 z-5" />
         <div className="absolute inset-0 bg-[linear-gradient(0deg,#fff,transparent)] z-10" />
         <div className="w-full py-22.5 flex flex-col justify-center items-center text-center relative z-15">
+          {/* TODO: Add onClick handler */}
           <div className="mb-14 border border-divider rounded-xs py-3 px-4 bg-background flex items-center gap-2 text-on-background hover:bg-on-background hover:text-on-brand hover:cursor-pointer">
             <PartyPopperIcon className="w-5 h-5 transition-colors duration-200" />
             <span className="text-xl transition-colors duration-200">Introducing Duri: AI business operator with a collaborative workspace.</span>
@@ -39,11 +40,13 @@ function App() {
           <p className="mt-3.75 text-2xl text-on-background-secondary">An AI business operator for small and medium businesses.</p>
           <div className="mt-10 flex items-start gap-6">
             <div>
+              {/* TODO: Update href */}
               <a href="/">
                 <button type="button" className="text-on-brand bg-brand hover:bg-transparent hover:text-brand box-border border border-transparent hover:border-brand font-normal leading-5 rounded-xs text-2xl px-3 py-[5px] md:px-8 md:py-6 focus:outline-none transition-colors duration-500 cursor-pointer">Try for free</button>
               </a>
               <p className="mt-3 text-sm text-on-background-secondary">No credit card required.</p>
             </div>
+            {/* TODO: Update href */}
             <a href="/">
               <button type="button" className="text-on-background bg-transparent box-border border border-on-background hover:bg-on-background hover:text-on-brand font-normal leading-5 rounded-xs text-2xl px-3 py-[5px] md:px-8 md:py-6 focus:outline-none transition-colors duration-500 cursor-pointer">Log in</button>
             </a>

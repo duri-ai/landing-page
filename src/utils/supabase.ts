11b52e61c9189edce6e1d5d59bf52e459f9bb02e
Jaehong Lee <ja34luv@gmail.com>
2026-05-30 20:30:18 -0700

    feat: add pkce flow for auth to work in prod

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY as string;
+const supabaseKey = import.meta.env
+  .VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: { flowType: "pkce" },
+});

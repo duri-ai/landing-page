6506c3bdd2740b88194d7528c3de97899e64afdf
Jaehong Lee <ja34luv@gmail.com>
2026-05-30 20:36:56 -0700

    feat: redirect email login to account page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -30,7 +30,7 @@ export default function LoginPage() {
         if (error) {
             setError(error.message);
         } else {
-            navigate("/");
+            navigate("/account");
         }
     }
 
